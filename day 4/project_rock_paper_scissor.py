17bf20516656eedbeeb2df01aeae784101822f5c
AunKumar7092 <arun989.ak24@gmail.com>
2022-04-09 21:04:07 +0530

    day 5 commit

--- day 4/project_rock_paper_scissor.py
+++ day 4/project_rock_paper_scissor.py
@@ -26,25 +26,46 @@ scissors = '''
 ---.__(___)
 '''
 
-my_choice =int(input("What do you choose? Type 0 for Rock,1 for paper or 2 for scissors."))
-if my_choice == 0:
-    print(rock)
-elif my_choice == 1:
-    print(paper)
-elif my_choice == 2:
-    print(scissors)
+game_images = [rock, paper, scissors]
+
+my_choice =int(input("What do you choose? Type 0 for Rock,1 for paper or 2 for scissors.")) 
+
+print(" You chosen :")
+
+# below commented code will also work but not efficient
+# if my_choice == 0:
+#     print(rock)
+# elif my_choice == 1:
+#     print(paper)
+# elif my_choice == 2:
+#     print(scissors)
+# else:
+#     print("please choose the correct option")
+
+if my_choice > 3 and my_choice < 0:
+    print("You typed an invalid number ,you lose!")
 else:
-    print("please choose the correct option")
+    print(game_images[my_choice])
+
+
 
 computer_choice = random.randint(0,2)
-if computer_choice == 0:
-    print(rock)
-elif computer_choice == 1:
-    print(paper)
-elif computer_choice == 2:
-    print(scissors)
+print(" Computer chosen :")
+# below commented code will also work but not efficient
+
+# if computer_choice == 0:
+#     print(rock)
+# elif computer_choice == 1:
+#     print(paper)
+# elif computer_choice == 2:
+#     print(scissors)
+# else:
+#     print("please choose the correct option")
+
+if computer_choice > 3 and computer_choice < 0:
+    print("You typed an invalid number ,you lose!")
 else:
-    print("please choose the correct option")
+    print(game_images[computer_choice])
 
 
 if my_choice == 0 and computer_choice == 2:
