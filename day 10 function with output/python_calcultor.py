0add8b2d462c0da8b3d8cf647ca47bd86f6c7fc3
Arunkumar7092 <arun989.ak24@gmail.com>
2022-04-16 20:10:21 +0530

    day 10 calculator project

---
+++ day 10 function with output/python_calcultor.py
@@ -0,0 +1,43 @@
+import os
+from art import logo
+print(logo)
+
+def add(num1 ,num2):
+    return num1 + num2
+
+def subtract(num1 ,num2):
+    return num1 - num2
+
+def multiply(num1 ,num2):
+    return num1 * num2
+
+def division(num1 ,num2):
+    return num1 / num2
+
+def function_recurion():
+    print(logo)
+    num1 = int(input(("what is the first number :")))
+    continue_calculation = True
+    while continue_calculation:
+        operators ={
+            "+" : add,
+            "-" : subtract,
+            "*" : multiply,
+            "/" : division
+            }
+        for symbol in operators:
+            print(symbol)
+
+        calculate_operator = operators[input("Pick an operation from the above line:  ").title()]
+        num2 = int(input(("what is the another number :")))
+        answer = calculate_operator(num1,num2)
+        print(answer)
+
+        if input("select 'Y' to continue operation with same calculated no or 'N' to start new calculation : ").lower() == 'y':
+            num1 = answer
+        else:
+            continue_calculation = False
+            os.system('clear')
+continue_new_calculation = True
+while continue_new_calculation:
+    function_recurion()
